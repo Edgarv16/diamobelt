63aa2cb0e4eb87a9ddfe1c086be1bbcf15177e89
eangamarca <informarys86@gmail.com>
2023-05-23 00:32:45 -0500

    commit clientes

---
+++ diamobelt/Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace diamobelt.Models
+{
+    public class Usuarios
+    {
+        public string nombresApellidos { get; set; }
+
+        public string username { get; set; }
+
+        public string contrasena { get; set; }
+
+        public string email { get; set; }
+        public string estado { get; set; }
+    }
+}
