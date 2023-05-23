8123f947b6445213260a6df0e11f9b906dda6019
Edgarv16 <edgv16@hotmail.com>
2023-05-22 23:07:52 -0500

    Agregar archivos de proyecto.

---
+++ diamobelt/Views/Loguin.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+using Xamarin.Forms.Xaml;
+
+namespace diamobelt
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Loguin : ContentPage
+    {
+        public Loguin()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void btnLoguin_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imgbotonUbicanos_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Views.Mapa());
+
+
+           
+        }
+    }
+}
---
+++ diamobelt/Views/Mapa.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+using Xamarin.Forms.Xaml;
+
+namespace diamobelt.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Mapa : ContentPage
+    {
+        public Mapa()
+        {
+            InitializeComponent();
+            Pin MiUbi = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Fabrica de Muebles Alexander",
+                Address = "Calle 27 de NOviembre y Francisco Teran",
+                Position = new Position(0.33469669703401445, -78.16981902586791), // 0.33469669703401445, -78.16981902586791
+                Tag = "id_miubi",
+            };
+            map.Pins.Add(MiUbi);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(MiUbi.Position, Distance.FromMeters(500)));
+        }
+    }
+}
