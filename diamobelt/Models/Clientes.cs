a2223ae412783b624ca6a64fb124cbe9f95b7bb5
eangamarca <informarys86@gmail.com>
2023-05-23 00:15:04 -0500

    agregar Modelo Clientes

---
+++ diamobelt/Models/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace diamobelt.Models
+{
+    public class Clientes
+    {
+     
+        public string cedula { get; set; }
+        public string nombresApellidos { get; set; }
+        public string telefono { get; set; }
+        public string correo { get; set; }
+        public string direccion { get; set; }
+        public string estado { get; set; }
+    }
+    
+}
